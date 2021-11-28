d868a76ba98c2dcedefe6405182f55c5f448f9c6
BryonVerdone <bryonverdone@gmail.com>
2021-11-27 21:53:54 -0500

    added footer section

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 //show sidebar
 
 const hideSidebar = document.querySelector('.hide-sidebar');
-
+const sidebar = document.querySelector('.sidebar');
 const navBtn = document.querySelector('.nav-btn');
 
+// navBtn.addEventListener('click', function () {
+//   hideSidebar.classList.toggle('hide-sidebar');
+// });
+// // close open sidebar
+// const closeNav = document.querySelector('.close-nav');
+
+// if (closeNav.classList.contains)
+//   closeNav.addEventListener('click', function () {
+//     closeNav.classList.add('hide-sidebar');
+//   });
+
 navBtn.addEventListener('click', function () {
-  hideSidebar.classList.toggle('hide-sidebar');
-});
-// close open sidebar
-const closeNav = document.querySelector('.close-nav');
-
-
-
-if(closeNav.classList.contains)
-
-closeNav.addEventListener('click', function () {
-  closeNav.classList.add('hide-sidebar');
+  if (sidebar.classList.contains('hide-sidebar')) {
+    sidebar.classList.remove('hide-sidebar');
+  } else {
+    sidebar.classList.add('hide-sidebar');
+  }
 });
